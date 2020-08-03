f7c388867ba9e90f498b9c95119e129303f7e4a3
darayuthhang <darayuthhang@atlassian.comhttps://darayuthhang@bitbucket.org>
2020-08-03 12:15:03 -0400

    Updates

---
+++ precache-manifest.e6c3f285b1e6b72d95c3cb30be1f21b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6f985c958140db95cd197da72685161",
+    "url": "/blog-react/index.html"
+  },
+  {
+    "revision": "44af041e033a2699b7c0",
+    "url": "/blog-react/static/css/2.d811125f.chunk.css"
+  },
+  {
+    "revision": "98970dfd337d9890afeb",
+    "url": "/blog-react/static/css/main.335046b1.chunk.css"
+  },
+  {
+    "revision": "44af041e033a2699b7c0",
+    "url": "/blog-react/static/js/2.d03fe05b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/blog-react/static/js/2.d03fe05b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98970dfd337d9890afeb",
+    "url": "/blog-react/static/js/main.2c102108.chunk.js"
+  },
+  {
+    "revision": "1036d163d515442c3f5a",
+    "url": "/blog-react/static/js/runtime-main.fe59f0cc.js"
+  }
+]);
